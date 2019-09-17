711dc1234b3872f9a7c97ce00c1add7c0c882890
Waxipola <marta.ruiz2@live.u-tad.com>
2019-09-17 10:30:57 +0200

    Programando

---
+++ Proyecto_Prueba/Assets/Personaje.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personaje : MonoBehaviour
+{
+    CharacterController cmpCC;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cmpCC = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cmpCC.Move(transform.forward * 10 * Time.deltaTime);
+    }
+}
